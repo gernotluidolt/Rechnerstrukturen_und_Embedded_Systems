00625b921b725c874f59bb79566120f7f6197459
gernot <gernot.luidolt@gmail.com>
2024-06-04 17:26:10 +0200

    ex02 Stand Abgabe

---
+++ src/ex_02/distance.cpp
@@ -0,0 +1,5 @@
+#include "distance.h"
+
+/*
+ * Ich habe meinen DistanceSensor mit der NewPing Bibliothek implementiert, NewPing.h und NewPing.cpp sind im JupyterHub hochgeladen.
+ */
---
+++ src/ex_02/distance.h
@@ -0,0 +1,13 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+/*
+ * Ich habe meinen DistanceSensor mit der NewPing Bibliothek implementiert, NewPing.h und NewPing.cpp sind im JupyterHub hochgeladen.
+ */
+
+class distance {
+
+};
+
+
+#endif //DISTANCE_H
